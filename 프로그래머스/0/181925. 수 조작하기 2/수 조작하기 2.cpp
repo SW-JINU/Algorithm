ff8015b39f6a113416d3b8d1594b095fcdf3b94a
SW-JINU <swjinu0125@gmail.com>
2025-06-18 16:36:28 +0900

    [level 0] Title: 수 조작하기 2, Time: 1.35 ms, Memory: 6.82 MB -BaekjoonHub

---
+++ 프로그래머스/0/181925. 수 조작하기 2/수 조작하기 2.cpp
@@ -0,0 +1,21 @@
+#include <string>
+#include <vector>
+
+using namespace std;
+
+string solution(vector<int> numLog) {
+    string answer = "";
+    int result = 0;
+    for (int i = 0; i < numLog.size() - 1; i++) {
+        if (numLog[i + 1] - numLog[i] == 1) {
+            answer += 'w';
+        } else if (numLog[i + 1] - numLog[i] == -1) {
+            answer += 's';
+        } else if (numLog[i + 1] - numLog[i] == 10) {
+            answer += 'd';
+        } else if (numLog[i + 1] - numLog[i] == -10) {
+            answer += 'a';
+        }
+    }
+    return answer;
+}
