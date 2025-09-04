659a735e7a2d5439ed0aea8b8dbcbaef666f5d63
SW-JINU <swjinu0125@gmail.com>
2025-09-04 15:31:41 +0900

    [Bronze V] Title: A+B, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/1000. A＋B/A＋B.cc
@@ -0,0 +1,11 @@
+#include <iostream>
+#include <cstdio>
+using namespace std;
+
+int main() {
+    int A, B;
+    scanf("%d %d", &A, &B);
+    int sum = A + B;
+    printf("%d\n", sum);
+    return 0;
+}
