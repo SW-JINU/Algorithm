7843651405d0c9a0718fe27f37c991e2f85d1bab
SW-JINU <swjinu0125@gmail.com>
2025-09-05 19:42:16 +0900

    [Bronze V] Title: A-B, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/1001. A－B/A－B.cc
@@ -0,0 +1,11 @@
+#include <iostream>
+#include <cstdio>
+using namespace std;
+
+int main() {
+    int A, B;
+    scanf("%d %d", &A, &B);
+    int result = A - B;
+    printf("%d\n", result);
+    return 0;
+}
