38cb4991029fcfc5bac8b052cfc67c5d12accb1c
SW-JINU <swjinu0125@gmail.com>
2025-09-06 16:10:56 +0900

    [Bronze V] Title: 사칙연산, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/10869. 사칙연산/사칙연산.cc
@@ -0,0 +1,19 @@
+#include <iostream>
+#include <cstdio>
+using namespace std;
+
+int main() {
+    int A, B;
+    scanf("%d %d", &A, &B);
+    int result1 = A + B;
+    printf("%d\n", result1);
+    int result2 = A - B;
+    printf("%d\n", result2);
+    int result3 = A * B;
+    printf("%d\n", result3);
+    int result4 = A / B;
+    printf("%d\n", result4);
+    int result5 = A % B;
+    printf("%d\n", result5);
+    return 0;
+}
