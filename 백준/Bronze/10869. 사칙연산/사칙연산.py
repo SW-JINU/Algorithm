909f00e4f169ad8c3c49912c269f09d9cb7a32d7
SW-JINU <swjinu0125@gmail.com>
2025-09-06 15:50:01 +0900

    [Bronze V] Title: 사칙연산, Time: 40 ms, Memory: 32412 KB -BaekjoonHub

---
+++ 백준/Bronze/10869. 사칙연산/사칙연산.py
@@ -0,0 +1,19 @@
+T = list(map(int, input().split()))
+
+A = T[0]
+B = T[1]
+
+result1 = A + B
+print(result1)
+
+result2 = A - B
+print(result2)
+
+result3 = A * B
+print(result3)
+
+result4 = A // B
+print(result4)
+
+result5 = A % B
+print(result5)
