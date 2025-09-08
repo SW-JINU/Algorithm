24af27a3d737afe8ecda4e945363ec6f80beda72
SW-JINU <swjinu0125@gmail.com>
2025-09-08 16:17:21 +0900

    [Bronze V] Title: 1998년생인 내가 태국에서는 2541년생?!, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/18108. 1998년생인 내가 태국에서는 2541년생？！/1998년생인 내가 태국에서는 2541년생？！.cc
@@ -0,0 +1,11 @@
+#include <iostream>
+#include <cstdio>
+using namespace std;
+
+int main() {
+    int A;
+    scanf("%d", &A);
+    int result = A - 543;
+    printf("%d\n", result);
+    return 0;
+}
