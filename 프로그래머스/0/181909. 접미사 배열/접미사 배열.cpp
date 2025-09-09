130321309aa896dd9d7db690d22fc13e04b929e3
SW-JINU <swjinu0125@gmail.com>
2025-09-09 19:21:21 +0900

    [level 0] Title: 접미사 배열, Time: 0.07 ms, Memory: 4.14 MB -BaekjoonHub

---
+++ 프로그래머스/0/181909. 접미사 배열/접미사 배열.cpp
@@ -0,0 +1,18 @@
+#include <string>
+#include <vector>
+#include <algorithm>
+
+using namespace std;
+
+vector<string> solution(string my_string) {
+    vector<string> answer;
+    int len = my_string.size();
+
+    for (int i = 1; i <= len; i++) {
+        answer.push_back(my_string.substr(len - i, i));
+    }
+
+    sort(answer.begin(), answer.end());
+
+    return answer;
+}
