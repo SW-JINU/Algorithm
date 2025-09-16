202636afff5577ddaf814b3c0340112b27497b8a
SW-JINU <swjinu0125@gmail.com>
2025-09-16 16:13:00 +0900

    [level 0] Title: qr code, Time: 0.03 ms, Memory: 4.27 MB -BaekjoonHub

---
+++ 프로그래머스/0/181903. qr code/qr code.cpp
@@ -0,0 +1,15 @@
+#include <string>
+#include <vector>
+
+using namespace std;
+
+string solution(int q, int r, string code) {
+    string answer = "";
+    int len = code.size();
+    for (int i = 0; i < len; i++) {
+        if (i % q == r){
+            answer += code[i];
+        }
+    }
+    return answer;
+}
