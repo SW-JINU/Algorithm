7ab660923c54da4ef0cb330a89cef65414076166
SW-JINU <swjinu0125@gmail.com>
2025-11-30 17:05:34 +0900

    [Silver IV] Title: 다음 소수, Time: 132 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Silver/4134. 다음 소수/다음 소수.cc
@@ -0,0 +1,38 @@
+#include <iostream>
+using namespace std;
+
+bool isPrime(long long int num) {
+  if (num == 0 || num == 1)
+    return false;
+
+  if(num == 2) return true;
+  else if (num % 2 == 0)
+    return false;
+  
+  for (long long int i = 3; i * i <= num; i += 2) {
+    if (num % i == 0) {
+      return false;
+    }
+  }
+
+  return true;
+}
+
+long long targetPrime(long long num) {
+  // 0과 1인 경우를 특정
+  while (!isPrime(num)) {
+    num++;
+  }
+  return num;
+}
+
+int main() {
+  int testNum;
+  cin >> testNum;
+
+  long long inputNum;
+  for (int i = 0; i < testNum; i++) {
+    cin >> inputNum;
+    cout << targetPrime(inputNum) << endl;
+  }
+}
